be1017136e0f6e51a74504bf72ea7d812496b590
Aryan Gupta <202051038@iiitvadodara.ac.in>
2023-04-29 16:18:25 +0530

    add feature for putting code in file

--- python-chatgpt-project.py
+++ python-chatgpt-project.py
@@ -1,7 +1,18 @@
 import requests
+import argparse
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument("prompt", help="prompt that pass to openAI")
+parser.add_argument("filename", help="prompt that pass to openAI")
+args = parser.parse_args()
+
 
 api_url = "https://api.openai.com/v1/completions"
-api_key = "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"
+
+api_key = os.getenv("OPENAI")
+
+print(api_key)
 
 request_headers = {
     "Content-Type" : "application/json",
@@ -10,7 +21,7 @@ request_headers = {
 
 request_body ={
     "model": "text-davinci-003",
-    "prompt": "Write the python script for writing hello world",
+    "prompt": f"Write the python script for {args.prompt}. Only Code not text",
     "max_tokens": 100,
     "temperature": 0
 }
@@ -20,6 +31,9 @@ response = requests.post(api_url,headers=request_headers,json=request_body)
  
 
 if response.status_code == 200:
-   print(f"response: {response.json()}")
+   text = response.json()['choices'][0]['text']
+   print(f"{text}")
+   with open(args.filename,'w') as file:
+       file.write(text)
 else:
-   print(f"Some error occured: {response.status_code}")   
+   print(f"Some error occured: {response.json()}")   
